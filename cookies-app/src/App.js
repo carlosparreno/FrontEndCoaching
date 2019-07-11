3975ac5b0c176e2b88ecd7ae9742dd12dd68c058
carlos-parreno <carlos.parreno@hotmail.es>
2019-07-11 19:26:37 +0100

    :new: Add new cookies app

---
+++ cookies-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <label>Cookies App</label>
+      </header>
+      <body />
+    </div>
+  );
+}
+
+export default App;
